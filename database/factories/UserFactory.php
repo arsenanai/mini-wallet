9690550707ff0490c9b2c1fa4e0983f4d6a24112
Arsen Anay <arsenanai@gmail.com>
2025-11-29 15:22:39 +0500

    factories and seeders are ready

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -28,6 +30,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'balance' => fake()->randomFloat(4, 1000, 10000),
             'remember_token' => Str::random(10),
         ];
     }
